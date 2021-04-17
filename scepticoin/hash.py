fb683ad6c3679a29177c6a6ab9fd2ce5102a7689
Sashimi Houdini <sashimi.houdini@protonmail.com>
2021-04-17 20:29:42 +0200

    Document the choice of scrypt parameters

--- scepticoin/hash.py
+++ scepticoin/hash.py
@@ -7,6 +7,18 @@ def sha256d(b):
 
 
 def scrypt(password, salt):
+    # Why did we choose the scrypt factors that we did? Well... we at least bothered to read the abstract of the scrypt
+    # paper, which is apparently more than the creators of Litecoin (and all its clones) did. As such, we tried to tune
+    # the parameters using the super-scientific method of timing it our our local development machines, aiming for
+    # 100ms.  N= 1 << 15 turned out to be the magic number, as guessed correctly by mr. Percival himself:
+    # https://github.com/golang/go/issues/22082#issuecomment-332983728
+
+    # Having said that, remember that ancient truth of crypto-currency: everything you see is shoddily built, since you
+    # can always paper over your mistakes with technobabble. Present coin included.
+
+    # More (fun) reading:
+    # https://bitcoin.stackexchange.com/questions/36642/why-did-litecoin-choose-the-scrypt-factors-that-they-did
+
     # buflen 32 was chosen... because scrypt's output is going to through sha256d anyway, so no sense in a greater
     # output space
     return scrypt_hash(password, salt, N=1 << 15, r=8, p=1, buflen=32)
