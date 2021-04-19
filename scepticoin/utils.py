039e07739ea9d5a2934358d38d6a7380252f5c3c
Sashimi Houdini <sashimi.houdini@protonmail.com>
2021-04-19 21:47:13 +0200

    Add calc_work utility
    Not actually used in the code, but useful to get a sense of the amount of
    waiste that's required to mine a block

--- scepticoin/utils.py
+++ scepticoin/utils.py
@@ -3,3 +3,7 @@ from .humans import human
 
 def block_filename(block):
     return "%08d-%s" % (block.height, human(block.hash()))
+
+
+def calc_work(target):
+    return pow(2, 32 * 8) // int.from_bytes(target, byteorder="big", signed=False)
