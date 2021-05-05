c5a9fc8c2f2144b9134eb522bdb6f35993f74868
marekjedrzejewski <marekjedrzejewski@users.noreply.github.com>
2021-05-05 10:21:24 +0200

    Fix configure_logging_from_args
    needed hasattr instead of args.name

--- skepticoin/scripts/utils.py
+++ skepticoin/scripts/utils.py
@@ -109,8 +109,8 @@ def configure_logging_for_stdout():
 
 
 def configure_logging_from_args(args):
-    if args.log_to_file:
+    if hasattr(args, 'log_to_file'):
         configure_logging_for_file()
 
-    if args.log_to_stdout:
+    if hasattr(args, 'log_to_stdout'):
         configure_logging_for_stdout()
