skepticoin/skepticoin

commit b411bc77bc01efd211cb0f88d0923f08830a2ab0

Improve commandline option handling
Refactoring, and add logging options to most commands

skepticoin/scripts/receive.py
@@ -2,13 +2,20 @@ import argparse
 
 from ..humans import human
 
-from .utils import open_or_init_wallet, save_wallet
+from .utils import (
+    open_or_init_wallet,
+    save_wallet,
+    configure_logging_from_args,
+    DefaultArgumentParser,
+)
 
 
 def main():
+    parser = DefaultArgumentParser()
     parser = argparse.ArgumentParser()
     parser.add_argument("annotation", help="Some text to help you remember a meaning for this receive address.")
     args = parser.parse_args()
+    configure_logging_from_args(args)
 
     wallet = open_or_init_wallet()
     public_key = wallet.get_annotated_public_key(args.annotation)

skepticoin/scripts/mine.py
@@ -16,12 +16,15 @@ from .utils import (
     open_or_init_wallet,
     start_networking_peer_in_background,
     check_for_fresh_chain,
-    configure_logging_by_argv,
+    configure_logging_from_args,
+    DefaultArgumentParser,
 )
 
 
 def main():
-    configure_logging_by_argv()
+    parser = DefaultArgumentParser()
+    args = parser.parse_args()
+    configure_logging_from_args(args)
 
     create_chain_dir()
     coinstate = read_chain_from_disk()

skepticoin/scripts/repl.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 
 from ptpython.repl import embed, run_config
 from ptpython.entry_points.run_ptpython import get_config_and_history_file
@@ -18,6 +17,8 @@ from .utils import (
     read_chain_from_disk,
     open_or_init_wallet,
     start_networking_peer_in_background,
+    configure_logging_from_args,
+    DefaultArgumentParser,
 )
 
 
@@ -29,9 +30,10 @@ class EverythingIsNone:
 def main():
     config_file, history_file = get_config_and_history_file(EverythingIsNone())
 
-    parser = argparse.ArgumentParser()
+    parser = DefaultArgumentParser()
     parser.add_argument("--vi-mode", help="Vi mode", action="store_true")
     args = parser.parse_args()
+    configure_logging_from_args(args)
 
     create_chain_dir()
     coinstate = read_chain_from_disk()

skepticoin/scripts/send.py
@@ -1,5 +1,4 @@
 from time import sleep
-import argparse
 
 from skepticoin.params import SASHIMI_PER_COIN
 from skepticoin.signing import SECP256k1PublicKey
@@ -13,15 +12,18 @@ from .utils import (
     open_or_init_wallet,
     start_networking_peer_in_background,
     check_for_fresh_chain,
+    configure_logging_from_args,
+    DefaultArgumentParser,
 )
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = DefaultArgumentParser()
     parser.add_argument("amount", help="The amount of to send", type=int)
     parser.add_argument("denomination", help="'skepticoin' or 'sashimi'", choices=['skepticoin', 'sashimi'])
     parser.add_argument("address", help="The address to send to")
     args = parser.parse_args()
+    configure_logging_from_args(args)
 
     value = args.amount * (SASHIMI_PER_COIN if args.denomination == 'skepticoin' else 1)
 

skepticoin/scripts/utils.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import os
 import tempfile
 import logging
+import argparse
 
 from skepticoin.datatypes import Block
 from skepticoin.coinstate import CoinState
@@ -13,6 +14,14 @@ from skepticoin.networking.utils import load_peers
 from skepticoin.params import DESIRED_BLOCK_TIMESPAN
 
 from time import time, sleep
+
+
+class DefaultArgumentParser(argparse.ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_argument("--log-to-file", help="Log to file", action="store_true")
+        self.add_argument("--log-to-stdout", help="Log to stdout", action="store_true")
 
 
 def initialize_peers_file():
@@ -96,9 +105,9 @@ def configure_logging_for_stdout():
     logging.basicConfig(format=FORMAT, stream=sys.stdout, level=logging.INFO)
 
 
-def configure_logging_by_argv():
-    if "--log-networking-to-file" in sys.argv:
+def configure_logging_from_args(args):
+    if args.log_to_file:
         configure_logging_for_file()
 
-    if "--log-networking-to-stdout" in sys.argv:
+    if args.log_to_stdout:
         configure_logging_for_stdout()

commit d9d4d206e4b087b3f2ddcbcd284a7aedad1e3e34

Fix skepticoin-receive command
Problem introduced in b411bc77bc01efd211cb0f88d0923f08830a2ab0

How this command could be in a broken state for so long, without anyone
noticing, is left as an excercise for the reader.

See #2

skepticoin/scripts/receive.py
@@ -1,5 +1,3 @@
-import argparse
-
 from ..humans import human
 
 from .utils import (
@@ -12,7 +10,6 @@ from .utils import (
 
 def main():
     parser = DefaultArgumentParser()
-    parser = argparse.ArgumentParser()
     parser.add_argument("annotation", help="Some text to help you remember a meaning for this receive address.")
     args = parser.parse_args()
     configure_logging_from_args(args)