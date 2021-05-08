60f5146e21557b6ef7fd7dac6c9e46b68b99dc30
Luuk Verweij <luuk_verweij@msn.com>
2021-05-08 15:43:31 +0200

    wip: adding annotations

--- skepticoin/signing.py
+++ skepticoin/signing.py
@@ -6,7 +6,11 @@ Maximum removal of smoke and mirrors. This means:
 
 One byte is reserved as a type_indicator if we ever want to offer more options (compressed public keys anyone?)
 """
+from __future__ import annotations
+
 import struct
+from io import BytesIO
+from typing import Any
 
 import ecdsa  # NOTE "This library was not designed with security in mind."
 
@@ -20,7 +24,7 @@ TYPE_SECP256k1 = b"\x02"
 
 class PublicKey(Serializable):
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> SECP256k1PublicKey:
         type_indicator = safe_read(f, 1)
 
         if type_indicator == TYPE_SECP256k1:
@@ -32,35 +36,36 @@ class PublicKey(Serializable):
 class SECP256k1PublicKey(PublicKey):
     """We use the same curve as bitcoin because why not. Remember: the NIST curves were chosen by the lizard people!"""
 
-    def __init__(self, public_key):
+    def __init__(self, public_key: bytes):
         if not len(public_key) == 64:
             raise ValueError("SECP256k1 public key must be 64 bytes.")
 
         self.public_key = public_key
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "SECP256k1 Public Key %s" % human(self.public_key)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return (
             isinstance(other, SECP256k1PublicKey)
             and self.public_key == other.public_key
         )
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.serialize())
 
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> SECP256k1PublicKey:
         # type_indicator has been read already by the superclass at this point.
-        public_key = safe_read(f, 64)
+        public_key: bytes = safe_read(f, 64)
         return cls(public_key)
 
-    def stream_serialize(self, f):
+    def stream_serialize(self, f: BytesIO) -> None:
         f.write(TYPE_SECP256k1)
         f.write(self.public_key)
 
-    def validate(self, signature, message):
+    def validate(self, signature: Any, message: bytes) -> bool:
+        breakpoint()
         if not isinstance(signature, SECP256k1Signature):
             return False
 
@@ -74,7 +79,7 @@ class SECP256k1PublicKey(PublicKey):
 
 class Signature(Serializable):
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> Signature:
         type_indicator = safe_read(f, 1)
 
         if type_indicator == TYPE_SIGNABLE_EQUIVALENT:
@@ -88,7 +93,7 @@ class Signature(Serializable):
 
         raise DeserializationError("Non-supported signature type.")
 
-    def is_not_signature(self):
+    def is_not_signature(self) -> bool:
         """In various places where signatures are expected, special-meaning placeholders can occur instead. Signatures
         that may actually be used to verify public keys should return False here."""
         return True
@@ -97,18 +102,18 @@ class Signature(Serializable):
 class SignableEquivalent(Signature):
     """SignableEquivalent: when signing transactions you can't sign your own signature."""
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "SignableEquivalent()"
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return isinstance(other, SignableEquivalent)
 
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> SignableEquivalent:
         # type_indicator has been read already by the superclass at this point.
         return cls()
 
-    def stream_serialize(self, f):
+    def stream_serialize(self, f: BytesIO) -> None:
         f.write(TYPE_SIGNABLE_EQUIVALENT)
 
 
@@ -116,9 +121,9 @@ class CoinbaseData(Signature):
     """In Coinbase transactions, some random data takes the place of the signature. This may be used by miners to
     introduce extra randomness if the nonce is not enough, or to include pseudo-polical messages."""
 
-    def __init__(self, height, signature):
+    def __init__(self, height: int, signature: bytes):
         if not (0 <= height <= 0xFFFFFFFF):
-            raise ValueError("CoinbaseData height out of range." % height)
+            raise ValueError("CoinbaseData height %d out of range." % height)
 
         if len(signature) > 256:
             raise ValueError("Unserializable CoinbaseData")
@@ -128,7 +133,7 @@ class CoinbaseData(Signature):
         self.height = height
         self.signature = signature
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if all([32 <= b < 127 for b in self.signature]):
             return 'CoinbaseData(%s, "%s")' % (
                 self.height,
@@ -136,18 +141,18 @@ class CoinbaseData(Signature):
             )
         return "CoinbaseData(%s, #%s)" % (self.height, human(self.signature))
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return isinstance(other, CoinbaseData) and self.signature == other.signature
 
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> CoinbaseData:
         # type_indicator has been read already by the superclass at this point.
         (height,) = struct.unpack(b">I", safe_read(f, 4))
         (length,) = struct.unpack(b"B", safe_read(f, 1))
         signature = safe_read(f, length)
         return cls(height, signature)
 
-    def stream_serialize(self, f):
+    def stream_serialize(self, f: BytesIO) -> None:
         f.write(TYPE_COINBASE_DATA)
         f.write(struct.pack(b">I", self.height))
         f.write(struct.pack(b"B", len(self.signature)))
@@ -155,37 +160,37 @@ class CoinbaseData(Signature):
 
 
 class SECP256k1Signature(Signature):
-    def __init__(self, signature):
+    def __init__(self, signature: bytes):
         if not len(signature) == 64:
             raise ValueError("SECP256k1 signature must be 64 bytes.")
 
         self.signature = signature
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "SECP256k1 Signature %s" % human(self.signature)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return (
             isinstance(other, SECP256k1Signature) and self.signature == other.signature
         )
 
     @classmethod
-    def stream_deserialize(cls, f):
+    def stream_deserialize(cls, f: BytesIO) -> SECP256k1Signature:
         # type_indicator has been read already by the superclass at this point.
         signature = safe_read(f, 64)
         return cls(signature)
 
-    def stream_serialize(self, f):
+    def stream_serialize(self, f: BytesIO) -> None:
         f.write(TYPE_SECP256k1)
         f.write(self.signature)
 
-    def validate(self, public_key, message):
+    def validate(self, public_key: Any, message: bytes) -> bool:
         if not isinstance(public_key, SECP256k1PublicKey):
             return False
 
         return public_key.validate(self, message)
 
-    def is_not_signature(self):
+    def is_not_signature(self) -> bool:
         return False
 
 
