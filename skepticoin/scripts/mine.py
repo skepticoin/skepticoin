4274489f443e94eaecd924a9b2f2b41ab015b6fa
D <dend456@gmail.com>
2021-05-11 15:26:54 -0400

    add multiprocessing option to mine script

--- skepticoin/scripts/mine.py
+++ skepticoin/scripts/mine.py
@@ -9,6 +9,7 @@ from skepticoin.wallet import save_wallet
 from skepticoin.utils import block_filename
 from skepticoin.cheating import MAX_KNOWN_HASH_HEIGHT
 from time import time
+from multiprocessing import Process, Lock
 
 from .utils import (
     initialize_peers_file,
@@ -22,14 +23,16 @@ from .utils import (
 )
 
 
-def main():
-    parser = DefaultArgumentParser()
-    args = parser.parse_args()
+def miner(args, wallet_lock):
     configure_logging_from_args(args)
 
     create_chain_dir()
     coinstate = read_chain_from_disk()
+
+    wallet_lock.acquire()
     wallet = open_or_init_wallet()
+    wallet_lock.release()
+
     initialize_peers_file()
     thread = start_networking_peer_in_background(args, coinstate)
     thread.local_peer.show_stats()
@@ -47,8 +50,10 @@ def main():
         print("Starting main loop")
 
         while True:
+            wallet_lock.acquire()
             public_key = wallet.get_annotated_public_key("reserved for potentially mined block")
             save_wallet(wallet)
+            wallet_lock.release()
 
             nonce = random.randrange(1 << 32)
             last_round_second = int(time())
@@ -73,8 +78,11 @@ def main():
             coinstate = coinstate.add_block(block, int(time()))
             with open(Path('chain') / block_filename(block), 'wb') as f:
                 f.write(block.serialize())
+
             print("FOUND", block_filename(block))
+            wallet_lock.acquire()
             print("Wallet balance: %s skepticoin" % (wallet.get_balance(coinstate) / Decimal(SASHIMI_PER_COIN)))
+            wallet_lock.release()
 
             thread.local_peer.chain_manager.set_coinstate(coinstate)
             thread.local_peer.network_manager.broadcast_block(block)
@@ -87,3 +95,30 @@ def main():
         print("Waiting for networking thread to stop")
         thread.join()
         print("Done; waiting for Python-exit")
+
+
+def main():
+    parser = DefaultArgumentParser()
+    parser.add_argument('-n', default=1, type=int, help='number of miner instances')
+    args = parser.parse_args()
+
+    wallet_lock = Lock()
+    if args.n > 1:
+        pids = [None] * args.n
+
+        for i in range(args.n):
+            if i > 0:
+                args.dont_listen = True
+            pids[i] = Process(target=miner, daemon=True, args=(args, wallet_lock))
+            pids[i].start()
+
+        try:
+            for pid in pids:
+                pid.join()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            for pid in pids:
+                pid.join()
+    else:
+        miner(args, wallet_lock)
